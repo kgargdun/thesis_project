0e3078496662e0c88ab93d8eb166bb33b799412a
Kaustubh Garg <kgargdun@gmail.com>
2021-11-30 12:34:17 +0530

    func correc

--- detect_drowsiness.py
+++ detect_drowsiness.py
@@ -1,5 +1,3 @@
-# python drowsiness_yawn.py --webcam 0 --alarm alarm.wav
-
 from scipy.spatial import distance as dist
 from imutils.video import VideoStream
 from imutils import face_utils
@@ -22,58 +20,51 @@ auth_token = 'XXX'
 client = Client(account_sid, auth_token)
 
 
-def sound_alarm(path):
-    playsound.playsound('alarm.wav', False)
-
-
 def eye_aspect_ratio(captured_eye):
     a = dist.euclidean(captured_eye[1], captured_eye[5])
     b = dist.euclidean(captured_eye[2], captured_eye[4])
     c = dist.euclidean(captured_eye[0], captured_eye[3])
 
-    ear = (a + b) / (2.0 * c)
-    return ear
+    calc_ear = (a + b) / (2.0 * c)
+    return calc_ear
 
 
-def final_ear(shape):
-    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+def calculate_ear(outline):
+    (l_start, l_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (r_start, r_end) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-    leftEye = shape[lStart:lEnd]
-    rightEye = shape[rStart:rEnd]
+    left_eye = outline[l_start:l_end]
+    right_eye = outline[r_start:r_end]
 
-    leftEAR = eye_aspect_ratio(leftEye)
-    rightEAR = eye_aspect_ratio(rightEye)
+    left_ear = eye_aspect_ratio(left_eye)
+    right_ear = eye_aspect_ratio(right_eye)
 
-    ear = (leftEAR + rightEAR) / 2.0
-    return (ear, leftEye, rightEye)
+    calc_ear = (left_ear + right_ear) / 2.0
+    return calc_ear, left_eye, right_eye
+
+
+def ring_bell():
+    playsound.playsound('alarm.wav', False)
 
 
 ap = argparse.ArgumentParser()
-##ap.add_argument("-p", "--shape-predictor", required=True, help="path to facial landmark predictor")
 ap.add_argument("-a", "--alarm", type=str, default="", help="path alarm .WAV file")
 ap.add_argument("-w", "--webcam", type=int, default=0, help="index of webcam on system")
 args = vars(ap.parse_args())
 
-EYE_AR_THRESH = 0.3
-EYE_AR_CONSEC_FRAMES = 30
-YAWN_THRESH = 20
-sms_eye = 1
-sms_yawn = 1
+EYE_AR_THRESH = 0.25
+EYE_AR_CONSEC_FRAMES = 25
+message_eye = 1
 
-# initialize the frame counter as well as a boolean used to
-# indicate if the alarm is going off
 COUNTER = 0
 ALARM_ON = False
 
-print("-> Loading the predictor and detector...")
-# detector = dlib.get_frontal_face_detector()
-detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")  # Faster but less accurate
+print("-> Starting up...")
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 print("-> Starting Video Stream")
 vs = VideoStream(src=args["webcam"]).start()
-# vs= VideoStream(usePiCamera=True).start()       //For Raspberry Pi
 time.sleep(1.0)
 
 while True:
@@ -82,17 +73,16 @@ while True:
     frame = imutils.resize(frame, width=450)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # rects = detector(gray, 0)
-    rects = detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
-                                      flags=cv2.CASCADE_SCALE_IMAGE)
+    rectangle = detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
+                                          flags=cv2.CASCADE_SCALE_IMAGE)
 
-    for (x, y, w, h) in rects:
+    for (x, y, w, h) in rectangle:
         rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
 
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
 
-        eye = final_ear(shape)
+        eye = calculate_ear(shape)
         ear = eye[0]
         leftEye = eye[1]
         rightEye = eye[2]
@@ -111,7 +101,7 @@ while True:
             if COUNTER >= EYE_AR_CONSEC_FRAMES:
                 if not ALARM_ON:
                     ALARM_ON = True
-                    if (sms_eye == 1):
+                    if message_eye == 1:
                         message = client.messages \
                             .create(
                             body="Drowsy driver detected at location " + str(g.latlng),
@@ -119,11 +109,11 @@ while True:
                             to='XXX'
                         )
                         print(message.sid)
-                        sms_eye = 0
+                        message_eye = 0
                         print('SMS Sent for drowsy')
 
                     if args["alarm"] != "":
-                        t = Thread(target=sound_alarm, args=(args["alarm"],))
+                        t = Thread(target=ring_bell, args=(args["alarm"],))
                         t.deamon = True
                         t.start()
 
@@ -132,7 +122,7 @@ while True:
 
         else:
             COUNTER = 0
-            sms_eye = 1
+            message_eye = 1
             ALARM_ON = False
 
         cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
